3bf64141f6e15d92dd4229a09a48c1825d379a16
opet <opet@Academico.edu>
2018-05-17 11:02:55 -0300

    11h02 17/05/2018

---
+++ source/MaisCadastros/DAL/MaisCadastrosContext.cs
@@ -0,0 +1,23 @@
+﻿using DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace DAL
+{
+    public class MaisCadastrosContext : DbContext
+    {
+        public MaisCadastrosContext(DbContextOptions<MaisCadastrosContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Funcionario> Funcionarios { get; set; }
+        public DbSet<Loja> Lojas { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Venda> Vendas { get; set; }
+        public DbSet<Setor> Setores { get; set; }
+        public DbSet<Marca> Marcas { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+    }
+}
---
+++ source/MaisCadastros/DAL/Models/Funcionario.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DAL.Models
+{
+    public class Funcionario
+    {
+        public long FuncionarioId { get; set; }
+
+        public string Name { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime DataNascimento { get; set; }
+
+        public string Endereco { get; set; }
+
+        public long Telefone { get; set; }
+
+        public string Email { get; set; }
+
+        public long CPF { get; set; }
+
+        public Boolean Gerente { get; set; }
+
+        public long? LojaId { get; set; }
+        public Loja Loja { get; set; }
+    }
+}
